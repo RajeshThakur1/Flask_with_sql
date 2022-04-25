ba496f8be66e35a6447ae8e72c82f35ee06027f2
RajeshThakur1 <rajesh@isl.net.in>
2022-04-25 21:25:54 +0530

    basic changes

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,8 @@ from app.main.util.decorator import token_required
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-          title='Generic Flask Api Boilerplate',
-          version='2.0',
+          title='Ecomerce Backend APIs',
+          version='1.0',
           description='Api Links are provided down below',
           authorizations=authorizations,
           )
