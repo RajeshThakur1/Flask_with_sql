c04725e48497c0f358f0ab83e7d45128d702d3c9
RajeshThakur1 <rajeshthakur1r@gmail.com>
2022-05-02 19:06:47 +0530

    superadmin created

--- app/main/model/superadmin.py
+++ app/main/model/superadmin.py
@@ -20,3 +20,14 @@ class SuperAdmin(db.Model):
     created_at = db.Column(db.DateTime, default= db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default= db.func.current_timestamp(), onupdate= db.func.current_timestamp())
     deleted_at = db.Column(db.DateTime, nullable=True)
+
+    @property
+    def password(self):
+        raise AttributeError("password: write-only field")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
